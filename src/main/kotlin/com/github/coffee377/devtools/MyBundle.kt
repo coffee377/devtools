b2ecd745dbf8c112b4cd9cdf6664bd296087acc3
GitHub Action <action@github.com>
2022-08-31 12:37:14 +0000

    Template cleanup

--- src/main/kotlin/com/github/coffee377/devtools/MyBundle.kt
+++ src/main/kotlin/com/github/coffee377/devtools/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.coffee377.devtools
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/test/kotlin/com/github/coffee377/devtools/MyPluginTest.kt
+++ src/test/kotlin/com/github/coffee377/devtools/MyPluginTest.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.coffee377.devtools
 
 import com.intellij.ide.highlighter.XmlFileType
 import com.intellij.psi.xml.XmlFile
